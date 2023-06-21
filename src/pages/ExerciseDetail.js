d4378e6d8f63fae237ec6fcac14ec2126ff5bc4a
paul <paulnyoiken@gmail.com>
2023-06-21 19:05:31 +0300

    add similarexercise details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -35,6 +35,7 @@ const ExerciseDetail = () => {
 
     fetchExercisesData();
   }, [id]);
+  if (!exerciseDetail) return <div>No Data</div>;
 
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
